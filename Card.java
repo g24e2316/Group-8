e6e2df5964b0e25f5d6f96eca1ba5ae3f2d01bd3
Lelethu-gif <lelethuconjwayo@gmail.com>
2024-09-15 16:30:44 +0200

    Add files via upload
    I am  not sure if we  should create another class for distributing the cards to the players , will figure it out today though.  except for the above so far only created a window for the cards( graphics) but i do not know how to combine everything together .  hmmm... hopefully i figure it out or u guys help.

---
+++ Card.java
@@ -0,0 +1,23 @@
+// this class defines a regular card ( like how it looks)
+import java.util.*;
+public class Card {
+	private int face;
+	private int value;
+
+	public Card (int face , int value) {
+		this.face= face;
+		this.value = value;
+	}
+	public int getface() {
+		return face;
+	}
+	public int getvalue() {
+		return value;
+	}
+	public String toString() {
+		String [] faces = { "Clubs " , "Diamonds","Hearts","Spades"};
+		String [] values= {null ,"Ace" , "2" ," 3" , "4" , "5" , "6" ,"7" , "8", "9", "10", "Jack"};
+		String ans =  values[this.value] + " of " + faces[this.face];
+		return ans;
+	}
+}
---
+++ Deck.java
@@ -0,0 +1,35 @@
+// this class creates the deck of 41 cards and shuffles them
+import java.util.*;
+
+public class Deck {
+	private ArrayList<Card> cards;
+	
+	public Deck () {
+		cards = new ArrayList<>();  
+		
+		for ( int i =0 ; i < 4 ; i ++ ) {         // looping though faces
+			for ( int k =1 ; k <=11 ; k ++ ) {   // looping though values
+				 
+				cards.add(new Card (i ,k )); // i add a new type of object of the face (i) and value(k)
+			
+			}
+		}
+		//so this will go ahead and create a club of aces , a club of two's , a club of 3's e.t.c
+	
+	// to shuffle ( from stack verflow)
+	Collections.shuffle(cards);
+    }
+	// a getter method to get the cards from the arraylist
+	public ArrayList<Card> getCards () {
+		return cards;
+	}
+	// this part i saw on the internet , feel free to modify
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Card card : cards) {
+            sb.append(card.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+    
+}	
