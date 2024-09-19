17265e7c748428d61e9817a4f7eef4856891c7e4
Lelethu-gif <lelethuconjwayo@gmail.com>
2024-09-19 18:31:20 +0200

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,87 @@
+import java.util.*;
+public class Player {
+	private String name ;        // player name like player 1 , player ,2, player 3 et.c
+	private ArrayList<Card> hand;  // arraylist of Card type that will be in the player's hands
+	
+	// a constructor that has parameters
+	// will set the name
+	// will set the hand to a new arraylist
+	public Player( String name ) {
+		this.name = name;
+		this.hand = new ArrayList<>();
+	}
+	// a getter method to get the name of the player
+	public String getName() {
+		return name;
+	}
+	// a getter method to get the cards from the player's hands
+	public  ArrayList<Card> getcards() {
+		return hand;
+	}
+	// this method will add the cards onto the players hand
+	public void addcards ( Card card ) {
+		hand.add(card);
+	}
+	
+	// this method will remove the cards from the players hand
+	public void removecards( Card card) {
+		hand.remove ( card) ;
+	}
+	
+	// this method will compare the current card with the next card of the same player
+	// if they are the same , it removes both of them
+	public void matchingcards(Card that) {
+		for ( int i =0 ; i < hand.size(); i ++) {
+			
+			boolean matched = false;
+			
+			for ( int j =1 ; j<hand.size() ; j ++) {
+				
+				if( hand.get(i).equals(hand.get(j))) {
+					hand.remove(i);
+					hand.remove(j);
+					matched = true;
+					break;
+				} else {
+					continue;
+				}
+			
+			}
+	    }
+	}
+	
+	public  void passcards ( Player anotherplayer) {
+		if ( !hand.isEmpty() ) {
+			Card card = hand.remove(0);
+			anotherplayer.addcards(card);
+		}
+	}
+	// this method check if the player has the jack of spades card
+	// but before it makes sure that its the last card the player is left with
+	// its giving loser
+
+	public boolean Loser() {
+		if ( hand.size() == 1) {
+			Card lastCard = hand.get(0); 
+            return lastCard.toString().equalsIgnoreCase("Jack of Spades");
+	    } else {
+			return false;
+		}
+	
+	}
+	
+	
+	// this method handles situation where the are no longer cards in the player's hands
+	// its giving winner
+	
+	public boolean nocards() {
+		return hand.isEmpty();
+	}
+}
+
+	
+	
+				
+		
+	
+		
